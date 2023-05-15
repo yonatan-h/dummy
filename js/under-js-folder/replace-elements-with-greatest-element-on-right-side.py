9eda04c29d1b890434775fc8cfdacd81e20f8487
yonatan-h <99685585+yonatan-h@users.noreply.github.com>
2023-05-15 16:45:03 +0300

    Uploaded By Mirkusve

---
+++ js/under-js-folder/replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,1 @@
+class Solution:    def replaceElements(self, nums: List[int]) -> List[int]:                current_max = -1                for i in reversed(range(len(nums))):            num = nums[i]            nums[i] = current_max                        current_max = max(current_max, num)                    return nums            
