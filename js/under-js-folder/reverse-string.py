e6842038eef58e641fd5522c5c58a3188edd7691
yonatan-h <99685585+yonatan-h@users.noreply.github.com>
2023-05-15 15:26:18 +0300

    Uploaded By Mirkusve

---
+++ js/under-js-folder/reverse-string.py
@@ -0,0 +1,1 @@
+class Solution:    def reverseString(self, s: List[str]) -> None:        swap(s, 0, len(s)-1)def swap(letters, left, right):    if left < right:        letters[left], letters[right] = letters[right], letters[left]        swap(letters, left+1, right-1)
