f8439f5a6195ff4aa3d9f57d42747de31cc17619
yonatan-h <99685585+yonatan-h@users.noreply.github.com>
2023-05-15 17:06:03 +0300

    Uploaded By Mirkusve

---
+++ js/under-js-folder/find-target-indices-after-sorting-array.py
@@ -0,0 +1,1 @@
+class Solution:    def targetIndices(self, nums: List[int], target: int) -> List[int]:        nums.sort()                indexes = []        for i in range(len(nums)):            num = nums[i]            if num == target:                indexes.append(i)        return indexes    #10min#1sub
